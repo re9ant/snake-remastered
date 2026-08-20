e8ee2423e1fe03b7ccc1a623e057dc6d561f6007
Kowshik <saikowsikayyalasomayajula@gmail.com>
2026-08-21 01:52:21 +0530

    snake moves and spawns bodies

--- scripts/snakeController.gd
+++ scripts/snakeController.gd
@@ -1,19 +1,16 @@
 extends Node2D
 
-enum Direction{
-	LEFT,
-	RIGHT,
-	UP,
-	DOWN
-}
-
-@export var snakeBody : Sprite2D
+@export var snakeBodyContainer : Node2D
+@export var snakeBodyScene : PackedScene
 
 var speed = 3
-var currDir = Direction.UP
+var currMoveDir = Vector2(0, 1)
+var bodyOffset = 130
+var currBodies = 0
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
+	currBodies = 0
 	pass
 
 
@@ -23,28 +20,25 @@ func _process(delta: float) -> void:
 	var verticalIp = round(Input.get_axis("ui_down", "ui_up"))
 	
 	if(abs(horizontalIp) > 0):
-		currDir = Direction.RIGHT if (horizontalIp > 0) else Direction.LEFT
+		currMoveDir = Vector2(1, 0) if (horizontalIp > 0) else Vector2(-1, 0)
 	if(abs(verticalIp) > 0):
-		currDir = Direction.UP if (verticalIp > 0) else Direction.DOWN
+		currMoveDir = Vector2(0, -1) if (verticalIp > 0) else Vector2(0, 1)
+		
 	updateSnakePos()
+	if(Input.is_action_just_pressed("ui_accept")):
+		addSnakeBody()
 	pass
 
 func addSnakeBody() -> void:
-	#snakeBody
+	currBodies += 1
+	var spawnedBody = snakeBodyScene.instantiate() as Sprite2D
+	snakeBodyContainer.add_child(spawnedBody)
+	var spawnPos = global_position
+	spawnPos -= currMoveDir * (bodyOffset * currBodies)
+	spawnedBody.global_position = spawnPos
+	print(spawnPos)
 	pass
 
 func updateSnakePos() -> void:
-	match currDir:
-		Direction.LEFT:
-			position.x -= speed
-			pass
-		Direction.RIGHT:
-			position.x += speed
-			pass
-		Direction.UP:
-			position.y -= speed
-			pass
-		Direction.DOWN:
-			position.y += speed
-			pass
+	position += currMoveDir * speed
 	pass
